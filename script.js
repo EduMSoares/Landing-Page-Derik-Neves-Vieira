e565b257f083ed86dcb8fcf3aa241d986d3c5332
Eduardo Magalhães Soares <90562184+EduMSoares@users.noreply.github.com>
2022-02-10 18:49:55 +0000

    Para disposivos mobile como tablets e celulares o menu do estilo navbar some e aparece um menu offcanvas (como estamos acostumados no mobile). La o menu esta um pouco diferente com um dropdown com os contatos

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ txtCopy.addEventListener('click', (e) => {
   
   alert('Texto copiado para área de transferência! Ctrl+V em algum local para colar');
 });
+
+//animação foto perfil derik
+
+const fotoPerfil = document.querySelector('img.primario-img')
+
+fotoPerfil.addEventListener('mouseenter', function () {
+
+  this.style.width = '430px'
+  
+})
+fotoPerfil.addEventListener('mouseout', function () {
+
+  this.style.width = '362px'
+  
+})

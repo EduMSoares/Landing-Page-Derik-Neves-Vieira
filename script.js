635cf5d350e86df856bf2d6ff7d75a018f180aaa
Eduardo Magalhães Soares <90562184+EduMSoares@users.noreply.github.com>
2022-02-12 02:09:03 +0000

    function Copiar numero de telefone concertada.

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // COPIAR NUMERO DE TELEFONE
-const txtCopy = document.querySelector('input#copytxt');
+const txtCopy = document.querySelector('input#copytxt1');
+const txtCopy2 = document.querySelector('input#copytxt2');
+
 txtCopy.addEventListener('click', (e) => {
   e.preventDefault();
   
   navigator.clipboard.writeText(txtCopy.value);
+  
+  alert('Texto copiado para área de transferência! Ctrl+V em algum local para colar');
+});
+
+txtCopy2.addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  navigator.clipboard.writeText(txtCopy2.value);
   
   alert('Texto copiado para área de transferência! Ctrl+V em algum local para colar');
 });
